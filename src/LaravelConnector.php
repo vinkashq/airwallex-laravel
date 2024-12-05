d440ad1dfd615efc5b3a68a67eaa44f181fb5c1b
Vinoth Kannan <svkn.87@gmail.com>
2024-12-05 12:17:18 +0530

    FIX: use correct token key get the value from cache.
    And get client id and api key from Laravel config.

--- src/LaravelConnector.php
+++ src/LaravelConnector.php
@@ -3,6 +3,7 @@
 namespace Vinkas\Airwallex;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
 use Vinkas\Airwallex\Connector;
 
 class LaravelConnector extends Connector
@@ -14,11 +15,21 @@ class LaravelConnector extends Connector
 
     public function getToken(): string
     {
-        return Cache::get([static::TOKEN_KEY]);
+        return Cache::get(static::TOKEN_KEY);
     }
 
     public function setToken(string $token): void
     {
         Cache::put(static::TOKEN_KEY, $token, now()->addMinutes(30));
+    }
+
+    public function getClientId(): string
+    {
+        return Config::get('services.airwallex.client_id');
+    }
+
+    public function getApiKey(): string
+    {
+        return Config::get('services.airwallex.api_key');
     }
 }
